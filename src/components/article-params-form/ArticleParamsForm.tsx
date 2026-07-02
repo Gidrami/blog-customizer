c6b951192c987fefdeb33dbf42c468f32e5eba0e
Alexandra Gorokhova <Gidramizi@yandex.ru>
2026-07-02 21:58:50 +0300

    готово

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,151 @@
+import { useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select/Select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Text } from 'src/ui/text';
+
+import {
+	defaultArticleState,
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type Props = {
+	onApply: (state: typeof defaultArticleState) => void;
+};
+
+export const ArticleParamsForm = ({ onApply }: Props) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] = useState(defaultArticleState);
+	const toggleOpen = () => setIsOpen((prev) => !prev);
+	const wrapperRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				isOpen &&
+				wrapperRef.current &&
+				!wrapperRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onApply(formState);
+		setIsOpen(false);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onApply(defaultArticleState);
+	};
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
-		</>
+		<div ref={wrapperRef}>
+			<ArrowButton isOpen={isOpen} onClick={toggleOpen} />
+
+			{isOpen && (
+				<aside
+					className={clsx(styles.container, {
+						[styles.container_open]: isOpen,
+					})}>
+					<form className={styles.form} onSubmit={handleSubmit}>
+						<Text as='h2' size={31} weight={800} uppercase>
+							Задайте параметры
+						</Text>
+						<Select
+							title='Шрифт'
+							selected={formState.fontFamilyOption}
+							options={fontFamilyOptions}
+							onChange={(value) =>
+								setFormState((prev) => ({
+									...prev,
+									fontFamilyOption: value,
+								}))
+							}
+						/>
+
+						<RadioGroup
+							name='fontSizeOption'
+							title='Размер шрифта'
+							selected={formState.fontSizeOption}
+							options={fontSizeOptions}
+							onChange={(value) =>
+								setFormState((prev) => ({
+									...prev,
+									fontSizeOption: value,
+								}))
+							}
+						/>
+
+						<Select
+							title='Цвет шрифта'
+							selected={formState.fontColor}
+							options={fontColors}
+							onChange={(value) =>
+								setFormState((prev) => ({
+									...prev,
+									fontColor: value,
+								}))
+							}
+						/>
+
+						<Separator />
+
+						<Select
+							title='Цвет фона'
+							options={backgroundColors}
+							selected={formState.backgroundColor}
+							onChange={(value) =>
+								setFormState((prev) => ({
+									...prev,
+									backgroundColor: value,
+								}))
+							}
+						/>
+
+						<Select
+							title='Ширина контента'
+							options={contentWidthArr}
+							selected={formState.contentWidth}
+							onChange={(value) =>
+								setFormState((prev) => ({
+									...prev,
+									contentWidth: value,
+								}))
+							}
+						/>
+
+						<div className={styles.bottomContainer}>
+							<Button
+								title='Сбросить'
+								htmlType='reset'
+								type='clear'
+								onClick={handleReset}
+							/>
+
+							<Button title='Применить' htmlType='submit' type='apply' />
+						</div>
+					</form>
+				</aside>
+			)}
+		</div>
 	);
 };
